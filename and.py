d6fc5cd9a41e5cd8ebf1deb044935c50ff70b120
shino <shinosamuel94@gmail.com>
2021-09-19 21:54:15 +0400

    code organized

--- and.py
+++ and.py
@@ -3,25 +3,34 @@ from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1],
-}
+def main(data, eta, epochs, filename, plotfilename):
+    
 
-df = pd.DataFrame(AND)
+    df = pd.DataFrame(data)
 
-print(df)
+    print(df)
 
-X,y = prepare_data(df)
+    X,y = prepare_data(df)
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+    
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
+    model = Perceptron(eta, epochs)
+    model.fit(X, y)
 
-_ = model.total_loss()
+    _ = model.total_loss()
 
-save_model(model,filename="and.model")
-save_plot(df, "and.png", model)
+    save_model(model,filename= filename)
+    save_plot(df, plotfilename, model)
+
+if __name__ == '__main__': # << Entry Point
+
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+    
+    main(data = AND, eta = ETA , epochs =EPOCHS, filename = "and.model", plotfilename = "and.png" )
--- or.py
+++ or.py
@@ -3,25 +3,34 @@ from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
 
-OR = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,1,1,1],
-}
+def main(data, eta, epochs, filename, plotfilename):
+    
 
-df = pd.DataFrame(OR)
+    df = pd.DataFrame(data)
 
-print(df)
+    print(df)
 
-X,y = prepare_data(df)
+    X,y = prepare_data(df)
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+    
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
+    model = Perceptron(eta, epochs)
+    model.fit(X, y)
 
-_ = model.total_loss()
+    _ = model.total_loss()
 
-save_model(model,filename="or.model")
-save_plot(df, "or.png", model)
+    save_model(model,filename= filename)
+    save_plot(df, plotfilename, model)
+
+if __name__ == '__main__': # << Entry Point
+
+    OR = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,1,1,1],
+    }
+
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+    
+    main(data = OR, eta = ETA , epochs =EPOCHS, filename = "or.model", plotfilename = "or.png" )
